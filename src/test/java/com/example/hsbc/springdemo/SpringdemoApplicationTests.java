5d86ce9d1c5407b9a21ce32297cc57e4265fe7c6
aqiang <aqiangjx@163.com>
2024-12-18 19:42:18 +0800

    first commit

---
+++ src/test/java/com/example/hsbc/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hsbc.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
